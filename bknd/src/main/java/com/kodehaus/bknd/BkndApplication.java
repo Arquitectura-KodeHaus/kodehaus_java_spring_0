6ade559fdc1685404924af3560bd6ad857a6cdbd
ElAreaAl2 <juandiegoariasd@hotmail.com>
2025-10-29 12:53:11 -0500

    fix: mejorar component scan y habilitar actuator mappings para debugging

--- bknd/src/main/java/com/kodehaus/bknd/BkndApplication.java
+++ bknd/src/main/java/com/kodehaus/bknd/BkndApplication.java
@@ -3,12 +3,21 @@ package com.kodehaus.bknd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.kodehaus.bknd", "com.kodehaus.plaza"})
+@SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.kodehaus.bknd",
+    "com.kodehaus.plaza.controller",    // ✅ Controllers
+    "com.kodehaus.plaza.service",       // ✅ Services
+    "com.kodehaus.plaza.security",      // ✅ Security configs
+    "com.kodehaus.plaza.config"         // ✅ Configs
+})
 @EntityScan(basePackages = {"com.kodehaus.plaza.entity"})
 @EnableJpaRepositories(basePackages = {"com.kodehaus.plaza.repository"})
 public class BkndApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(BkndApplication.class, args);
     }
